422f48750d02aaff4d9883cc8f5d6f40df10eeca
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-13 15:36:57 +0200

    First fake component business code

---
+++ YieldVersion/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
+
+namespace Org.Kevoree.YieldVersion
+{
+	[ComponentType]
+	class YieldVersion
+	{
+
+		private readonly string Version = "1.0.0";
+
+		[Output]
+		private readonly Port tick;
+
+		[Start]
+		public void Start ()
+		{
+			Thread thd = new Thread (new ThreadStart (new YieldVersion ().Run));	
+			thd.Start ();
+		}
+
+		public void Run ()
+		{
+			while (true) {
+				Console.WriteLine (Version);
+				Thread.Sleep (1000);
+			}
+		}
+	}
+}
