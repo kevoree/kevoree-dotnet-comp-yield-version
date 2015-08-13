b981896f9b8bb63a267a41f86cd99f8934665d72
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-13 16:55:33 +0200

    First basic component code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Threading;
 using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
 
 namespace Org.Kevoree.YieldVersion
 {
+
+
+
+	[ComponentType]
     class YieldVersion
     {
 
+		[Output]
+		private Port port;
+
+		[Start]
+		public void Start() {
+			Thread t = new Thread(new ThreadStart(new YieldVersion().Run));
+
+
+			t.Start ();
+		}
+
+		public void Run() {
+			while (true) {
+				port.send("1.0.0", null);
+			}
+		}
     }
 }
