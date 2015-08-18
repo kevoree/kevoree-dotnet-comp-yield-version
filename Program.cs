165b46627a1149b57d298878b8cd453207df5994
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-18 10:48:02 +0200

    First version 1.0.0

--- Program.cs
+++ Program.cs
@@ -2,31 +2,52 @@
 using System.Threading;
 using Org.Kevoree.Annotation;
 using Org.Kevoree.Core.Api;
+using Org.Kevoree;
+
 
 namespace Org.Kevoree.YieldVersion
 {
 
-
-
 	[ComponentType]
-    class YieldVersion
-    {
+	class YieldVersion
+	{
+		private bool StopMe;
+		private readonly string VERSION = "1.0.0";
+		private Log.Log logger = Log.LogFactory.getLog (typeof(YieldVersion).ToString (), Log.Level.DEBUG, "YieldVersion");
 
 		[Output]
 		private Port port;
 
 		[Start]
-		public void Start() {
-			Thread t = new Thread(new ThreadStart(new YieldVersion().Run));
-
-
+		public void Start ()
+		{
+			StopMe = false;
+			Thread t = new Thread (new ThreadStart (new YieldVersion ().Run));
+			logger.Debug (VERSION + " - Start");
 			t.Start ();
 		}
 
-		public void Run() {
-			while (true) {
-				port.send("1.0.0", null);
-			}
+		[Stop]
+		public void Stop ()
+		{
+			this.StopMe = true;
+			logger.Debug (VERSION + " - Stop");
 		}
-    }
+
+		[Update]
+		public void Update ()
+		{
+			logger.Debug (VERSION + " - Update");
+		}
+
+		public void Run ()
+		{
+			logger.Debug (VERSION + " - Thread started");
+			while (!StopMe) {
+				port.send (VERSION, null);
+				logger.Debug (VERSION + " - YIELD");
+			}
+			logger.Debug (VERSION + " - Thread finished");
+		}
+	}
 }
